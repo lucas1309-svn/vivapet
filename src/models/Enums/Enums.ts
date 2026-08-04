babadba3fc2fd05b14020c950ef745d75acb3d91
Emanuel <emalucad10@gmail.com>
2026-08-04 16:57:25 -0300

    Foram criadas as classes Cuidador, RegistroUso e Necessidade e seus respectivos relacionamentos

---
+++ src/models/Enums/Enums.ts
@@ -0,0 +1,41 @@
+export enum TipoAnimal {
+
+    GATO = "GATO",
+    CACHORRO = "CACHORRO"
+
+}
+
+export enum SexoAnimal {
+
+    MACHO = "MACHO",
+    FEMEA = "FEMEA"
+
+}
+
+export enum StatusAnimal {
+
+    ATIVO = "ATIVO",
+    EM_ADOCAO = "EM_ADOCAO",
+    ADOTADO = "ADOTADO",
+    FALECIDO = "FALEIDO"
+
+}
+
+
+export enum TipoNecessidade {
+
+    MEDICAMENTO = "MEDICAMENTO.",
+    ALIMENTACAO = "ALIMENTACAO",
+    HIGIENE = "HIGIENE",
+    ENRIQUECIMENTO = "ENRIQUECIMENTO"
+}
+
+
+export enum TipoCuidador {
+
+    VETERINARIO = "VETERINARIO",
+    VOLUNTARIO = "VOLUNTÁRIO",
+    ESTAGIARIO = "ESTAGIÁRIO"
+    
+}
+
---
+++ src/models/RegistroUso.ts/RegistroUso.ts
@@ -0,0 +1,48 @@
+import { Animal } from "../Animal/Animal";
+import { Cuidador } from "../Cuidador/Cuidador";
+
+export class RegistroUso{
+
+    private readonly _id: string;
+    private readonly _dataUso: Date;
+    private readonly _quantidadeUsada: number;
+    private readonly _observacoes: string;
+
+    private readonly _cuidador: Cuidador;
+    private readonly _animal: Animal;
+
+    constructor(id: string, dataUso: Date, quantidadeUsada: number, observacoes: string, cuidador: Cuidador, animal: Animal){
+
+        this._id = id;
+        this._dataUso = dataUso;
+        this._quantidadeUsada = quantidadeUsada;
+        this._observacoes = observacoes;
+        this._cuidador = cuidador;
+        this._animal = animal
+    }
+
+
+    public get id(){
+        return this._id
+    }
+
+    public get dataUso(){
+        return this._dataUso
+    }
+
+    public get quantidadeUsada(){
+        return this._quantidadeUsada
+    }
+
+    public get observacoes(){
+        return this._observacoes
+    }
+
+    public get cuidador(){
+        return this._cuidador
+    }
+
+    public get animal(){
+        return this._animal
+    }
+}
